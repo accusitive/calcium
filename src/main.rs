ba54f068eb0a4a090071b8008d5769a7c6b7e4e7
RS <rs@neb.best>
2021-11-12 13:35:24 -0500

    AutoFMT

--- src/main.rs
+++ src/main.rs
@@ -159,3 +159,152 @@ fn main() {
         }
     }
 }
+#[cfg(test)]
+mod tests {
+    use ca_parser_bison::value::Value;
+    use paste::paste;
+    macro_rules! test_stmt {
+        ($i: ident, $v: expr) => {
+            paste! {
+                #[test]
+                fn [<stmt_ $i>]() {
+                    let v = $v;
+                    ca_ast::to_statement(v);
+                }
+                #[test]
+                #[should_panic]
+                fn [<expr_ $i>]() {
+                    let v = $v;
+                    ca_ast::to_expression(v);
+                }
+                #[test]
+                #[should_panic]
+                fn [<func_ $i>]() {
+                    let v = $v;
+                    ca_ast::to_function(v);
+                }
+            }
+        };
+    }
+    macro_rules! test_expr {
+        ($i: ident, $v: expr) => {
+            paste! {
+                #[test]
+                #[should_panic]
+                fn [<stmt_ $i>]() {
+                    let v = $v;
+                    ca_ast::to_statement(v);
+                }
+                #[test]
+                fn [<expr_ $i>]() {
+                    let v = $v;
+                    ca_ast::to_expression(v);
+                }
+                #[test]
+                #[should_panic]
+                fn [<func_ $i>]() {
+                    let v = $v;
+                    ca_ast::to_function(v);
+                }
+            }
+        };
+    }
+    test_stmt! { return_stmt, &Value::Statement(Box::new(Value::ReturnStatement(Box::new(Value::Expr(Box::new(Value::LiteralExpr("12".to_string())))))))}
+    test_stmt! { let_infer, &Value::Statement(Box::new(Value::LetStatement(Box::new(Value::Ident("someident".to_string())),Box::new(Value::Ty(Box::new(Value::Infer))), Box::new(Value::Expr(Box::new(Value::LiteralExpr("12".to_string())))))))}
+    test_stmt! { let_i32, &Value::Statement(Box::new(Value::LetStatement(Box::new(Value::Ident("someident".to_string())),Box::new(Value::Ty(Box::new(Value::PathExpr(Box::new(Value::ValueList(vec![Value::Ident("i32".to_string())])))))), Box::new(Value::Expr(Box::new(Value::LiteralExpr("12".to_string())))))))}
+    test_expr! {basic_expr, &Value::Expr(Box::new(Value::LiteralExpr("12".to_string())))}
+    use ca_ast::*;
+    macro_rules! asdf {
+        ($main: ident, $test_case: expr, $($i: ident),*) => {
+            paste! {
+                #[test]
+                fn [<asdf_ $main>]() {
+                    [<to_$main>]($test_case);
+                }
+                $(
+                    #[test]
+                    #[should_panic]
+                    fn [<$main _asdf_ $i>]() {
+                        [<to_$i>]($test_case);
+
+                    }
+                )*
+            }
+        };
+    }
+    // asdf!(program, function, expression, statement, function_arg, ty, path, indetifier, vec);
+    macro_rules! test_case_program {
+        () => {
+            Box::new(Value::Program(Box::new(Value::ValueList(vec![]))))
+        };
+    }
+    macro_rules! test_case_statement {
+        () => {
+            Box::new(Value::Statement(Box::new(Value::LetStatement(
+                test_case_ident!(),
+                test_case_ty!(),
+                test_case_expr!(),
+            ))))
+        };
+    }
+    macro_rules! test_case_ident {
+        () => {
+            Box::new(Value::Ident("test_case".to_string()))
+        };
+    }
+    macro_rules! test_case_ty {
+        () => {
+            Box::new(Value::Ty(Box::new(Value::Infer)))
+        };
+    }
+    macro_rules! test_case_expr {
+        () => {
+            Box::new(Value::Expr(Box::new(Value::LiteralExpr("12".to_string()))))
+        };
+    }
+
+    asdf!(
+        program,
+        &test_case_program!(),
+        function,
+        expression,
+        statement,
+        function_arg,
+        ty,
+        path,
+        identifier,
+        vec
+    );
+    asdf!(
+        statement,
+        &test_case_statement!(),
+        function,
+        expression,
+        function_arg,
+        ty,
+        path,
+        identifier,
+        vec
+    );
+    asdf!(
+        ty,
+        &test_case_ty!(),
+        function,
+        expression,
+        function_arg,
+        path,
+        identifier,
+        vec
+    );
+    asdf!(
+        expression,
+        &test_case_expr!(),
+        program,
+        function,
+        function_arg,
+        ty,
+        path,
+        identifier,
+        vec
+    );
+}
