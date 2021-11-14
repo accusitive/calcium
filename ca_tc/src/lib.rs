1c0293be8b1fe64d884429b5792de5117c6d3dd9
RS <rs@neb.best>
2021-11-14 17:40:53 -0500

    fmt

--- ca_tc/src/lib.rs
+++ ca_tc/src/lib.rs
@@ -5,19 +5,17 @@ use ca_uir::{Function, Item, Literal, Program, Statement, Struct, Ty};
 #[allow(dead_code)]
 pub struct TypeChecker {
     functions: RefCell<HashMap<String, Function>>,
-    structs: RefCell<HashMap<String, Struct>>
-
+    structs: RefCell<HashMap<String, Struct>>,
 }
 
 impl TypeChecker {
     pub fn new() -> Self {
         Self {
             functions: RefCell::new(HashMap::new()),
-            structs: RefCell::new(HashMap::new())
+            structs: RefCell::new(HashMap::new()),
         }
     }
     pub fn check_program(&self, p: &Program) {
-        
         for item in &p.items {
             self.check_item(item);
         }
@@ -29,33 +27,30 @@ impl TypeChecker {
             Item::Import(p) => self.check_program(&p.prog),
         }
     }
-    
+
     pub fn check_function(&self, f: &Function) {
         if f.is_extern {
-            return
+            return;
         }
         match f.body.as_ref().unwrap() {
             ca_uir::Expression::Block(stmts) => {
                 for stmt in stmts {
                     self.check_statement(stmt)
                 }
-            },
+            }
             _ => panic!(),
         }
     }
     pub fn check_statement(&self, s: &Statement) {
         match s {
             Statement::Let(_name, expected_ty, right) => {
-                
                 let right_ty = self.get_expr_ty(right);
                 if !expected_ty.is_infer() && &right_ty != expected_ty {
                     panic!("Let statement's right hand type is {}, but the right hand evaluates to {}.", right_ty, expected_ty);
                 }
-            },
-            Statement::Return(_) => {},
-            Statement::Expr(_e) => {
-    
-            },
+            }
+            Statement::Return(_) => {}
+            Statement::Expr(_e) => {}
         }
     }
     pub fn check_struct(&self, s: &Struct) {
@@ -66,7 +61,7 @@ impl TypeChecker {
             }
         }
     }
-    
+
     pub fn get_expr_ty(&self, e: &ca_uir::Expression) -> Ty {
         match e {
             ca_uir::Expression::Call(_, _) => todo!(),
